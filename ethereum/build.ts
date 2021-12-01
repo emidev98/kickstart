f67132521b787043424a0320ccd8da795952577f
Emilian Harastasan <eharastasan@gmail.com>
2021-12-01 07:23:29 +0100

    wip(refact): smart contracts from js to ts

---
+++ ethereum/build.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import fs from "fs";
+const solc = require("solc");
+
+const buildPath = path.resolve(__dirname, "build");
+
+try {
+    console.log("Start contract compilation");
+
+    fs.rmdirSync(buildPath, { recursive: true });
+    fs.mkdirSync(buildPath);
+    
+    const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+    const source = fs.readFileSync(campaignPath, "utf-8");
+    const output = solc.compile(source, 1).contracts;
+     
+    for (let contract in output) {
+        const contractName = contract.replace(":","");
+        const fileName = path.resolve(buildPath, contractName + ".json");
+        const contractObject = output[contract];
+
+        console.log(`'${contractName}' compiled successfully`)
+        
+        fs.writeFileSync(fileName, JSON.stringify(contractObject));
+    }
+
+    console.log("Contract compiled successfully");
+}
+catch(e){
+    console.error("Contract failed to compile " + e);
+}
---
+++ ethereum/web3.ts
@@ -0,0 +1,22 @@
+import Web3 from "web3";
+
+let web3;
+const isMetamaskAvailable = (<any> window).ethereum !== undefined;
+
+// If ethereum object exists on window object 
+// is because Metamask injected it.
+if (isMetamaskAvailable) {
+  (<any> window).ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3((<any> window).ethereum);
+} 
+// Otherwise we create our own provider for the 
+// user to at least be able to view the data
+else {
+  const provider = new Web3.providers.HttpProvider(
+    process.env["PROVIDER_URL"]
+  );
+  web3 = new Web3(provider);
+}
+
+export default web3;
+export { isMetamaskAvailable, web3 };
