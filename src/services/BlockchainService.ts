cc98293fd4efa8493585252b8a08c62b4d577e55
Emilian Harastasan <eharastasan@gmail.com>
2021-12-07 21:05:00 +0100

    wip(metamask): localstorage comparison with JSON file

--- src/services/BlockchainService.ts
+++ src/services/BlockchainService.ts
@@ -4,16 +4,46 @@ import LocalStorageService from "./LocalStorageService";
 import * as _ from "lodash";
 
 class BlockchainService {
-	static get blockchains(): Array<IBlockchain> {
-		const storage = LocalStorageService.get();
 
-		if(_.isEmpty(storage.blockchains)){
+	static get blockchains(): Array<IBlockchain> {
+		const storageChains = LocalStorageService.get().blockchains;
+
+		// If local storage is not used get all 
+		// blockchains from config file
+		if(_.isEmpty(storageChains)){
 			LocalStorageService.setItem("blockchains", defaultChains);
 			return defaultChains as Array<IBlockchain>;
 		}
 
-		// TODO compare default chains agains local storage chains
-		return storage.blockchains;
+		// Validate if all chains from local storage are up to date
+		// and delete the chains that are not existing anymore
+		const updatedChains = _.chain(storageChains) // FIX 
+			.map((storageChain) => {
+				const newChain = _.find(defaultChains, (defaultChain : IBlockchain) => {
+					const isSameChain = storageChain.chainId === defaultChain.chainId;
+					const isOutdated = storageChain.version < defaultChain.version;
+
+					return isSameChain && isOutdated;
+				});
+
+				return _.isUndefined(newChain) ? storageChain : newChain;
+			})
+			.filter((newChain) => {
+				return _.find(defaultChains, (defaultChain) => newChain.chainId === defaultChain.chainId);
+			})
+			.value();
+		
+		// Find if there is still a selected chain
+		const selectedChain = _.find(updatedChains, chain => chain.selected);
+
+		// If there is no selected chain use the first option
+		if(_.isUndefined(selectedChain)){
+			updatedChains[0].selected = true;
+		}
+
+		LocalStorageService.setItem("blockchains", updatedChains);
+
+		return updatedChains;
 	}
 
 	static select(chainId : string) {
